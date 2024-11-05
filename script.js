c32a3f37b11ac3c8ff14f8e14f8d87bb3898e0f1
codewithzubi <159233893+codewithzubi@users.noreply.github.com>
2024-11-05 08:55:29 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+    function toggleSection(sectionId) {
+      const section = document.getElementById(sectionId);
+      if (section.style.display === "none" || section.style.display === "") {
+          section.style.display = "block";
+      } else {
+          section.style.display = "none";
+      }
+    }
+
+
+    
